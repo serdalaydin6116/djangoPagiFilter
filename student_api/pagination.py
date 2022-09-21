5a3f51b6de7c28a466e904d613951641afe00ee1
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-21 22:16:26 +0300

    paginatıon

---
+++ student_api/pagination.py
@@ -0,0 +1,19 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+
+class SmallPageNumberPagination(PageNumberPagination):
+    page_size = 8
+    page_query_param = "sayfa"
+
+# Local Settings:
+from rest_framework.pagination import LimitOffsetPagination
+class MyLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 3
+    limit_query_param = 'adet'  
+
+##cursor paginbation
+from rest_framework.pagination import CursorPagination
+class MyCursorPagination(CursorPagination):
+    page_size = 10
+    ordering = 'id'  
